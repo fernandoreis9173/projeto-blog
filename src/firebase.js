d5491b6980dda055ebeb8edde46943281339cd72
fernando reis <fernandopw_15cellfaire@hotmail.com>
2020-02-21 23:28:58 -0400

    uploading post image

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
         let firebaseConfig = {
             apiKey: "AIzaSyAKfDjJ7156tuw5vFmNzQvfoVWYjpzlroY",
@@ -16,12 +17,18 @@ import 'firebase/auth';
 class firebase{
     constructor(){
         app.initializeApp(firebaseConfig);
-
+        // Referenciando a database para acessar em outros locais 
         this.app = app.database();
+
+        this.storage = app.storage();
     }
 
     login(email, password){
         return app. auth().signInWithEmailAndPassword(email, password)
+    }
+
+    logout(){
+        return app.auth().signOut();
     }
 
     async register(nome, email, password){
@@ -43,6 +50,20 @@ class firebase{
 
     getCurrent(){
         return app.auth().currentUser && app.auth().currentUser.email
+    }
+
+    getCurrentUid(){
+        return app.auth().currentUser && app.auth().currentUser.uid
+    }
+
+    async getUserName(callback){
+        if(!app.auth().currentUser){
+            return null;
+        }
+
+        const uid = app.auth().currentUser.uid;
+        await app.database().ref('usuarios').child(uid)
+        .once('value').then(callback);
     }
 }
     export default new firebase();
